b64f03a155ec078bec2286748138b83c8dd5fee5
majidzeno <majid.eltayeb@gmail.com>
2022-12-26 09:34:07 +0200

    fix: :adhesive_bandage: fix missing `key` in child components

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,23 +1,38 @@
-import { Alert, Form } from "react-bootstrap";
+/** @format */
+
+import { Alert, Form } from 'react-bootstrap';
 
 const Messages = ({ msgs }) => {
-  return (<div>
-    {
-      msgs.map((msg, i) => {
-        return <Alert variant={msg.variant} key={i}>
-          {msg.title && <b>{msg.title}: </b>}
-          {
-            msg.textarea ? <Form.Control className="output-area" as="textarea" value={msg.content} readOnly/> : <div> {renderMsg(msg)} </div>
-          }
-        </Alert>
-      })
-    }
-  </div>)
-}
+	return (
+		<div>
+			{msgs.map((msg, i) => {
+				return (
+					<Alert variant={msg.variant} key={i}>
+						{msg.title && <b>{msg.title}: </b>}
+						{msg.textarea ? (
+							<Form.Control
+								className='output-area'
+								as='textarea'
+                key={i}
+								value={msg.content}
+								readOnly
+							/>
+						) : (
+							<div key={i}> {renderMsg(msg, i)} </div>
+						)}
+					</Alert>
+				);
+			})}
+		</div>
+	);
+};
 
-const renderMsg = (msg) => (
-  Array.isArray(msg.content) ?
-              msg.content.map(c => <p className="msg-p">{c}</p>) :
-              <p className="msg-p">{msg.content}</p>
-)
+const renderMsg = (msg, i) =>
+	Array.isArray(msg.content) ? (
+		msg.content.map((c, i) => <p className='msg-p' key={i}>{c}</p>)
+	) : (
+		<p className='msg-p' key={i}>
+			{msg.content}
+		</p>
+	);
 export default Messages;
